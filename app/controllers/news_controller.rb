434f45dc656d7881651846e20194839a60945f6d
d.pishnenko <d.pishnenko@dunice.net>
2022-09-05 10:17:03 +0300

    Removed extra methods

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,15 +1,9 @@
 class NewsController < ApplicationController
-  before_action :set_news, only: %i[ show update destroy ]
 
   # GET /news
   def index
     @news = News.all
 
-    render json: @news
-  end
-
-  # GET /news/1
-  def show
     render json: @news
   end
 
@@ -24,26 +18,7 @@ class NewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /news/1
-  def update
-    if @news.update(news_params)
-      render json: @news
-    else
-      render json: @news.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /news/1
-  def destroy
-    @news.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_news
-      @news = News.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def news_params
       params.require(:news).permit(:title, :content, :authorId, :image, :tag)
