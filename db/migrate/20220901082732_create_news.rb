645ce3fd3a646e88d9f54811d5f7ebf803f7e1a4
d.pishnenko <d.pishnenko@dunice.net>
2022-09-05 12:44:32 +0300

    Change news and denylist migration

--- db/migrate/20220901082732_create_news.rb
+++ db/migrate/20220901082732_create_news.rb
@@ -1,9 +1,11 @@
 class CreateNews < ActiveRecord::Migration[7.0]
   def change
-    create_table :jwt_denylist do |t|
-      t.string :jti, null: false
-      t.datetime :exp, null: false
+    create_table :news do |t|
+      t.string "title"
+      t.string "content"
+      t.integer "authorId"
+      t.string "image"
+      t.string "tag"
     end
-    add_index :jwt_denylist, :jti
   end
 end
