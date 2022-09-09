76932864ad60c608e3780649e0d3d0cc4905c0e9
d.pishnenko <d.pishnenko@dunice.net>
2022-09-09 15:17:02 +0300

    Make migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_02_091115) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_084946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,8 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_091115) do
     t.integer "authorId"
     t.string "image"
     t.string "tag"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
