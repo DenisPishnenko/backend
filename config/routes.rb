423b49d8dddf9573d9e779689b7c9d58ebd1523d
d.pishnenko <d.pishnenko@dunice.net>
2022-09-09 15:14:06 +0300

    Add routes for sessions and registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, defaults: { format: :json },
+              controllers: {
+                sessions: 'users/sessions',
+                registrations: 'users/registrations'
+              }  
   resources :news
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
 
-  # Defines the root path route ("/")
-  root "news#index"
+  get '/member-data', to: 'members#show'
+  # root "news#index"
 end
