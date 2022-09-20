bc94fc682aba81c1e50e9ad420ee93f0b3844857
d.pishnenko <d.pishnenko@dunice.net>
2022-09-20 17:14:59 +0300

    Add authentication to show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController
   before_action :set_user
-  before_action :authenticate_user!, only: %i[ update ]
+  before_action :authenticate_user!, only: %i[ update show ]
                 
   respond_to :json
 
   def show
     if @user.nil?
-      render json: { message: 'User not found' }, status: :not_found
+      render json: { message: 'User not found' }, status: :not_found 
     else
-      render json: @user.to_json(include: :news)
+      render json: @user.to_json(include: :news), status: :ok
     end  
   end
 
   def update
-
     if @user.update(user_params)
-      render json: @user, status: :ok
+      render json: @user.to_json(include: :news), status: :ok
     else 
       render json: @user.errors, status: :unprocessable_entity
     end   
